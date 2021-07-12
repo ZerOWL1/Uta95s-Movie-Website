dc7417d09e39293ad942e3f09e97bd54178a3096
dathmhe141249 <80512109+dathmhe141249@users.noreply.github.com>
2021-07-12 22:05:41 +0700

    Add files via upload

---
+++ Models/Entity/Parent/Users.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Uta95s_Movie_Web___BETA_0._1.Models.Entity
+{
+    public class Users
+    {
+        private string name;
+        private string email;
+        private string password;
+        private int role;
+
+        public Users()
+        {
+        }
+
+        public Users(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+
+        public Users(string name, string email, string password, int role)
+        {
+            this.Name = name;
+            this.Email = email;
+            this.Password = password;
+            this.Role = role;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Email { get => email; set => email = value; }
+        public string Password { get => password; set => password = value; }
+        public int Role { get => role; set => role = value; }
+    }
+}
